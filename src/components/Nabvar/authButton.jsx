80b4b10dedf4958cc04a30f2c8802339868a3a2b
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-01-19 23:55:22 -0500

    Add button Iniciar Sesión

--- src/components/Nabvar/authButton.jsx
+++ src/components/Nabvar/authButton.jsx
@@ -1,5 +1,8 @@
 const AuthButton = () => {
-    return <div>AuthButtons</div>
+    return <div className="py-4 flex justify-center items-center gap-4 flex-wrap">AuthButtons
+    <button className="btn btn-neutral btn-outline">Crear Cuenta </button>
+    <div className="hidden lg:block">Iniciar Sesión</div>
+    </div>
 
 }
 
